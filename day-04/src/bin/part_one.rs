03f9ba7e48ffbe91a4990194a4505d2bb063a6e5
Abdelrahman Bahaa <abdelrahmanb015@gmail.com>
2024-12-07 04:19:40 +0200

    day 4

---
+++ day-04/src/bin/part_one.rs
@@ -0,0 +1,94 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    println!("{}", solve_day_one(&input));
+}
+
+fn solve_day_one(input: &str) -> usize {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let rows = grid.len();
+    let columns = grid[0].len();
+
+    let row_matches: usize = grid
+        .iter()
+        .map(|row| row.windows(4).filter(|slice| is_xmas(slice)).count())
+        .sum();
+
+    let column_matches: usize = (0..columns)
+        .map(|j| {
+            (3..rows)
+                .map(|i| [grid[i - 3][j], grid[i - 2][j], grid[i - 1][j], grid[i][j]])
+                .filter(|slice| is_xmas(slice))
+                .count()
+        })
+        .sum();
+
+    let diagonal_matches: usize = (3..rows)
+        .map(|i| {
+            (3..columns)
+                .map(|j| {
+                    [
+                        grid[i - 3][j - 3],
+                        grid[i - 2][j - 2],
+                        grid[i - 1][j - 1],
+                        grid[i][j],
+                    ]
+                })
+                .filter(|slice| is_xmas(slice))
+                .count()
+        })
+        .sum();
+
+    let reverse_diagonal_matches: usize = (3..rows)
+        .map(|i| {
+            (3..columns)
+                .rev()
+                .map(|j| {
+                    [
+                        grid[i - 3][j],
+                        grid[i - 2][j - 1],
+                        grid[i - 1][j - 2],
+                        grid[i][j - 3],
+                    ]
+                })
+                .filter(|slice| is_xmas(slice))
+                .count()
+        })
+        .sum();
+
+    return row_matches + column_matches + diagonal_matches + reverse_diagonal_matches;
+}
+
+fn is_xmas(slice: &[char]) -> bool {
+    if slice[0] == 'X' && slice[1] == 'M' && slice[2] == 'A' && slice[3] == 'S' {
+        return true;
+    }
+    if slice[3] == 'X' && slice[2] == 'M' && slice[1] == 'A' && slice[0] == 'S' {
+        return true;
+    }
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_day_one() {
+        let input = [
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        ]
+        .join("\n");
+        assert_eq!(solve_day_one(&input), 18);
+    }
+}
---
+++ day-04/src/bin/part_two.rs
@@ -0,0 +1,56 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let input = read_to_string("input.txt").unwrap();
+    println!("{}", solve_day_two(&input));
+}
+
+fn solve_day_two(input: &str) -> usize {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let rows = grid.len();
+    let columns = grid[0].len();
+    return (1..rows - 1)
+        .map(|i| (1..columns - 1).filter(|j| is_xmas(&grid, i, *j)).count())
+        .sum();
+}
+
+fn is_xmas(grid: &Vec<Vec<char>>, cx: usize, cy: usize) -> bool {
+    // M| |S
+    //  |A|
+    // M| |S
+    return is_mas(grid[cx - 1][cy - 1], grid[cx][cy], grid[cx + 1][cy + 1])
+        && is_mas(grid[cx - 1][cy + 1], grid[cx][cy], grid[cx + 1][cy - 1]);
+}
+
+fn is_mas(c0: char, c1: char, c2: char) -> bool {
+    if c0 == 'M' && c1 == 'A' && c2 == 'S' {
+        return true;
+    }
+    if c2 == 'M' && c1 == 'A' && c0 == 'S' {
+        return true;
+    }
+    return false;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve_day_two() {
+        let input = [
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX",
+        ]
+        .join("\n");
+        assert_eq!(solve_day_two(&input), 9);
+    }
+}
